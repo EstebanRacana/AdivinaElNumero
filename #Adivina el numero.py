bf7fdbd25fc5a802b4dbd8aeaadc2412db2f648d
Esteban <esteban.racana@gmail.com>
2026-02-16 19:01:16 -0300

    Modificaciones
    Se modifico las variables para que se ajuste el entendimiento del programa.

--- #Adivina el numero.py
+++ #Adivina el numero.py
@@ -1,19 +1,19 @@
 import random
-numeroRandom=random.randint(1,100)
-cont=3;
+numeroAleatorio=random.randint(1,100)
+intentos=3;
 #print(f"Random integer: {numeroRandom}")
 print("Estoy pensando en un numero del 1 al 100...")
-while(cont!=0):
-    numeroUsuario=int(input("Ingrese un numero para adivinar que numero piensa la maquina, tenes 3 intentos"))
-    if(numeroUsuario==numeroRandom):
+while(intentos!=0):
+    numeroUsuario=int(input("Ingrese un numero para adivinar que numero piensa la maquina, tenes 3 intentos: "))
+    if(numeroUsuario==numeroAleatorio):
         print("Lo lograste! Acertado.")
-        cont=0
-    elif(numeroUsuario>numeroRandom):
+        intentos=0
+    elif(numeroUsuario>numeroAleatorio):
             print("Te fuiste muy lejos, intenta un numero mas bajo")
-            cont=cont-1
-    elif(numeroUsuario<numeroRandom):
+            intentos=intentos-1
+    elif(numeroUsuario<numeroAleatorio):
             print("Te fuiste muy lejos, intenta un numero mas alto")
-            cont=cont-1
-    if(cont==0):
-        print("Juego finalizado, el numero era: ",numeroRandom)
+            intentos=intentos-1
+    if(intentos==0):
+        print("Juego finalizado, el numero era: ",numeroAleatorio)
         
